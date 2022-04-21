87c919187bcd9cea41fa286fe55b0663a00e5850
aditchandra24 <104150549+aditchandra24@users.noreply.github.com>
2022-04-21 22:45:47 +0700

    Add files via upload

---
+++ uts.py
@@ -0,0 +1,9 @@
+## 10 sampai 15
+print("Bilangan 10 sampai 15")
+for i in range(10, 16):
+  print('i =', i);
+
+## Bilangan genap kelipatan 2
+print("Bilangan Genap Kelipatan 2")
+for i in range(2, 12, 2):
+  print('i =', i);
